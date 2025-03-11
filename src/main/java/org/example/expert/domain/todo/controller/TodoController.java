2cf79f4ab5ef3d534d119473c71c0fdc90c70304
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-03-11 21:03:39 +0900

    ✨ Feat: Level.3 / 검색 기능 추가 1차

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -7,6 +7,7 @@ import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
 import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
+import org.example.expert.domain.todo.dto.response.TodoSearchResponse;
 import org.example.expert.domain.todo.service.TodoService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -37,6 +38,16 @@ public class TodoController {
             @RequestParam(required = false) LocalDate to
             ) {
         return ResponseEntity.ok(todoService.getTodos(page, size, weather, from, to));
+    }
+
+    @GetMapping("/todos/search")
+    public ResponseEntity<Page<TodoSearchResponse>> searchTodos(@RequestParam(defaultValue = "1") int page,
+                                                                @RequestParam(defaultValue = "10") int size,
+                                                                @RequestParam(required = false) String title,
+                                                                @RequestParam(required = false) LocalDate createAtFrom,
+                                                                @RequestParam(required = false) LocalDate createAtTo){
+        Page<TodoSearchResponse> todos = todoService.searchTodos(page, size, title, createAtFrom, createAtTo);
+        return ResponseEntity.ok(todos);
     }
 
     @GetMapping("/todos/{todoId}")
